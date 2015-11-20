ac9c1a50645e4e4c1d64dea78f1180e957512ecf
wott <william.ott@medicustek.com>
2015-11-20 16:07:38 +0800

    Solve exercise 3.4

--- ch03/DataStructures.scala
+++ ch03/DataStructures.scala
@@ -18,7 +18,7 @@ object List {
         if (as.isEmpty) Nil
         else Cons(as.head, apply(as.tail: _*))
 
-    // Excercise 3.1, Page 34
+    // Exercise 3.1, Page 34
     val x = List(1,2,3,4,5) match {
         case Cons(x, Cons(2, Cons(4, _))) => x
         case Nil => 42
@@ -27,19 +27,34 @@ object List {
         case _ => 101 // Also a match but won't be hit
     }
 
-    // Excercise 3.2, Page 35
+    // Exercise 3.2, Page 35
     def tail[A](l: List[A]): List[A] =
         l match {
             case Nil => sys.error("tail of empty list")
             case Cons(_, t) => t
         }
 
-    // Excercise 3.3, Page 36
+    // Exercise 3.3, Page 36
     def setHead[A](h: A, l: List[A]): List[A] =
         l match {
             case Nil => sys.error("setHead of empty list")
             case Cons(_, t) => Cons(h, t)
         }
+
+    // Exercise 3.4, Page 36
+    def drop[A](l: List[A], n: Int): List[A] = {
+      @annotation.tailrec
+      def loop(n: Int, l: List[A]): List[A] = {
+        if (n > 0) loop(n-1, tail(l))
+        else l
+      }
+
+      l match {
+        case Nil => sys.error("empty list")
+        case Cons(_, t) => loop(n-1, t)
+      }
+    }
+
 }
 
 object DataStructures {
@@ -47,5 +62,6 @@ object DataStructures {
         println(List.x)
         println(List.tail(List(1,2,3,4,5)))
         println(List.setHead(6, List(1,2,3,4,5)))
+        println(List.drop(List(1,2,3,4,5), 2))
     }
 }
